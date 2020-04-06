99c715c6d0d3b92327040d33fc166468054e6d2b
Jeff Huang <jhuang9356@gmail.com>
2020-04-06 00:17:56 -0400

    postgres

--- capstone/settings.py
+++ capstone/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jeff',
+        'USER': 'jeff',
+        'PASSWORD': '',
+        'HOST' : 'localhost',
+        'PORT' : '5432',    
     }
 }
 
@@ -130,3 +142,5 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+
--- capstone/wsgi.py
+++ capstone/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+# sys.path.append('/var/www/capstone/capstone')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
+
 
 application = get_wsgi_application()
