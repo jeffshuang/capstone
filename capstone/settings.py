e296cb55bc65141ee0572ab676e2f1a8a744e93c
jeffshuang <jhuang9356@gmail.com>
2020-04-21 23:30:36 -0400

    new version on new laptop currently reverted back to sqlite

--- capstone/settings.py
+++ capstone/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+}
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -92,7 +92,7 @@ DATABASES = {
         'HOST' : 'localhost',
         'PORT' : '5432',    
     }
-}
+}"""
 
 
 # Password validation
